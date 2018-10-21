267f5328578347671e6077ede1db03b52c38dc65
Gabriel Peres Magni <gpmagni@gmail.com>
2018-10-20 22:56:11 -0300

    Add files via upload

---
+++ horaDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,11 @@
+package horadosistema;
+import java.util.Date;
+
+public class HoraDoSistema {
+	public static void main(String[] args) {
+		Date data = new Date();
+		System.out.print("A data de hoje é: ");
+		System.out.println(data.toString());
+	}
+
+}
